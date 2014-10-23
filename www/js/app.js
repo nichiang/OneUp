c6def06d9951137ffb8c600039dbe32eb9f44fe9
nichiang <nick.chiang@gmail.com>
2014-10-23 00:05:35 -0700

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,29 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+app.controller('ListCtrl', function($scope){
+  $scope.goals = goals;
+});
+
+var goals = [
+    { title: 'Stop Smoking', progress: 0.35 },
+    { title: 'Daily Run', progress: 0.55 },
+    { title: 'App Development', progress: 0.8 }
+  ];
